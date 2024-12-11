ccc7eabd64e1534b3365bd0858a84d46b6b79906
Milan Sharma <milansharma5933@gmail.com>
2024-12-11 22:43:00 +0530

    Finally Ready For beta testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-// import HeroSection from "../components/home/HeroSection";
-// import LatestQuestions from "../components/home/LatestQuestions";
-// import TopContributers from "../components/home/TopContributers";
+import HeroSection from "../components/home/HeroSection";
+import LatestQuestions from "../components/home/LatestQuestions";
+import TopContributers from "../components/home/TopContributers";
 
 export default function Home() {
     return (
         <>
-            {/* <HeroSection /> */}
+            <HeroSection />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-12 items-start">
-                {/* <LatestQuestions /> */}
+                <LatestQuestions />
                 <div className="flex justify-center flex-col items-center py-5">
                     <span className="text-2xl pb-3">Top Contributers</span>
-                    {/* <TopContributers /> */}
+                    <TopContributers />
                 </div>
             </div>
         </>

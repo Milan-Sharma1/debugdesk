d9dde8ea08ac518845a24969f4f9d1b1c8c74138
Milan Sharma <milansharma5933@gmail.com>
2024-12-13 15:24:42 +0530

    feat: enhance password validation to include maximum length

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -6,5 +6,8 @@ export const registerSchema = z.object({
         .min(3, "name must be 3 letter minimum")
         .max(15, "Name must no longer than 15 chars"),
     email: z.string().email({ message: "Enter a valid email" }),
-    password: z.string().min(8, "Password must be 8 char long minimum"),
+    password: z
+        .string()
+        .min(8, "Password must be 8 char long minimum")
+        .max(50, "Password must be shorter than 50 chars"),
 });

cb4cbe6f879e1a9beb6cc6db3a5011047eca3b4a
Milan Sharma <milansharma5933@gmail.com>
2024-12-13 15:25:38 +0530

    feat: update password validation to enforce minimum and maximum length

--- src/schemas/loginSchema.ts
+++ src/schemas/loginSchema.ts
@@ -2,5 +2,8 @@ import { z } from "zod";
 
 export const loginValidation = z.object({
     identifier: z.string(),
-    password: z.string(),
+    password: z
+        .string()
+        .min(8, "Password must be 8 char long minimum")
+        .max(50, "Password must be shorter than 50 chars"),
 });
